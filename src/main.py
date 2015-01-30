3bb3e0acbb67a77855e10585751010aa024092e3
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-01-30 15:39:33 +0000

    added main class

---
+++ src/main.py
@@ -0,0 +1,38 @@
+
+import serialize
+import zmqServer
+import dbWriter
+import logging
+
+class Main:
+
+    def __init__(self):
+        self._opts = self.options()
+        
+    
+    def event(self, blob):
+        """Event callback on every received log message"""
+        ser = serialize.Serialize()
+        logrec = ser.deserialize(blob)
+        self.dbw.insertRecord(logrec)
+        
+        self.dbw.commit()
+    
+
+    def options(self):
+        # TODO: implement proper opt/arg parsing!!!
+        return {'logfile': '/tmp/sqlogz.sqlite'}
+
+    def run(self):
+        self.dbw = dbWriter.DBWriter(self._opts['file'])
+        self.server = zmqServer.ServerTask()
+        self.server.register_callback(self.event)
+        self.server.run()
+
+
+if __name__=="__main__":
+    main = Main()
+    main.run()
+
+        
+        
