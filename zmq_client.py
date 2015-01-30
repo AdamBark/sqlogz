b79883cad2b5871e15ffb7838c9fea70837f148f
Matthew Furseman <matthew.furseman@diamond.ac.uk>
2015-01-30 13:55:04 +0000

    Added draft of zmq client

---
+++ zmq_client.py
@@ -0,0 +1,14 @@
+import zmq
+
+class ClientTask(object):
+    def __init__(self, server, port=5555):
+        self.server = server
+        self.port = port
+
+    def send_log(log_item):
+        """Sends a serialized object to the connected server"""
+        pass
+
+    def connect():
+        """Connects to remove logz server"""
+        pass
