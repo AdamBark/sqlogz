4cc77d0d28aa354ada4fb58a583740ef3442dd8b
Matthew Furseman <matthew.furseman@diamond.ac.uk>
2015-01-30 14:16:10 +0000

    Added draft of zmq server

---
+++ src/zmqServer.py
@@ -0,0 +1,14 @@
+import zmq
+
+class ServerTask(object):
+    def __init__(self, port=5555):
+        self.port = port
+
+    def register_callback(callback_func):
+        """Register a function to be called with received strings"""
+        # callbacks.append(callback_func)
+        pass
+
+    def run(self):
+        """Start the server thread"""
+        pass
